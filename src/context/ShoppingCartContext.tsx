fa8c62932c53038b7e00983ac45b47af2b82c1a0
Odeke12 <trevodex@gmail.com>
2022-10-07 14:44:01 +0300

    Updated shopping cart context

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,7 +1,17 @@
-import { useContext } from "react";
+import { useContext, createContext, ReactNode } from "react";
 
 const ShoppingCartContext = createContext({})
 
 export function useShoppingCard(){
+    return useContext(ShoppingCartContext)
+}
 
+type ShoppingCartProviderProps = {
+    children: ReactNode
+}
+
+export function ShoppingCardProvider({children}: ShoppingCartProviderProps){
+    return <ShoppingCartContext.Provider value={{}}>
+        {children}
+    </ShoppingCartContext.Provider>
 }
